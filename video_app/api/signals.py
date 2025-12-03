22bf72299fec69d5a5974ab0d6b20eefc8013569
Marco Alber <m.alber@onlinehome.de>
2025-12-03 20:44:07 +0100

    remove comment

--- video_app/api/signals.py
+++ video_app/api/signals.py
@@ -1,10 +1,8 @@
-# video_app/api/signals.py
-
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from ..models import Video
 import django_rq
-from video_app.api.tasks import generate_thumbnail, generate_hls 
+from video_app.api.tasks import generate_thumbnail, generate_hls
 
 @receiver(post_save, sender=Video)
 def generate_thumbnail_and_hls_signal(sender, instance, created, **kwargs):
